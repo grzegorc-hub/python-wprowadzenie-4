4daa039f4c7f20f2a64543c8cd1caa3842799fb3
grzegorc <grzegorc@op.pl>
2020-07-07 19:50:23 +0200

    updated rozdzial 18

--- R18_funkcje_argumenty.py
+++ R18_funkcje_argumenty.py
@@ -1,4 +1,4 @@
-def changer(a, b):                       # Do argumentów przypisano referencje do obiektów
+def changer(a, b):                        # Do argumentów przypisano referencje do obiektów
     a = 2                                 # Zmiana wartości jedynie zmiennej lokalnej
     b[0] = 'mielonka'                     # Zmiana współdzielonego obiektu w miejscu
 
@@ -17,7 +17,9 @@ def multiple(x, y):
 X = 1
 L = [1, 2]
 X, L = multiple(X, L)                    # Przypisanie wyników do zmiennych wywołującego
+#X1, L1 = multiple(X, L)
 print(X, L)
+#print(X1, L1)
 
 def f(a,b,c): print(a, b, c)
 f(1,2,3)
@@ -37,12 +39,20 @@ f()
 f(a=1,b=3,c=9)
 def f(a, *pargs, **kargs): print(a, pargs, kargs)
 f(1,2,3,b=9,u=8)
+
 def func(a, b, c, d): print(a, b, c, d)
 func(*(1, 2), **{'d': 4, 'c': 4})
 T = (9,9,8,7)
 func(*T)
 
-def kwonly(a, *b, c):
+def kwonly(a, *b, c):            # c - keyword-only argument
     print(a, b, c)
 
-kwonly(1,2,c=3)
+#kwonly(1,2,3) # blad
+kwonly(1,2,c=3) # OK
+
+def kwonly2(a, *, b, c):
+    print(a, b, c)
+
+#kwonly2(11, 22, 33) # blad
+kwonly2(11, b=22, c=33) # OK
