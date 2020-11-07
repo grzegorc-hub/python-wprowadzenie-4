be0aabd4e9761cc245a08edf975995c140e35c1b
grzegorc <grzegorc@op.pl>
2020-11-07 23:26:42 +0100

    updated R15

--- R15_dokumentacja2.py
+++ R15_dokumentacja2.py
@@ -3,6 +3,7 @@ import R15_dokumentacja
 print(R15_dokumentacja.__doc__)
 print(R15_dokumentacja.square.__doc__)
 print(R15_dokumentacja.square(4).__doc__)
+# print(help(int))
 
 import sys
 print(sys.__doc__)
