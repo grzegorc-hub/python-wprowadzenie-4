bb8a9fb71947427eed22027ce3fb4d76584f5c56
Maciej <grzegorc@op.pl>
2022-10-01 19:31:48 +0200

    test push

--- R04_typy_obiektow.py
+++ R04_typy_obiektow.py
@@ -2,16 +2,16 @@ import random
 import math
 
 
-#=========#
+###########
 # liczby  #
-#=========#
+###########
 a = random.random()
 print(a, math.pi)
 
 
-#==================#
+####################
 # lancuchy znakow  #
-#==================#
+####################
 s = "mielonka"
 print(len(s), dir(s))
 print(s[1], s[-1])
@@ -42,25 +42,27 @@ print(next(G))
 print(next(G))
 
 
+print("""
 #############
 # slowniki  #
-#############
+#############""")
 D = {x: ord(x) for x in "aaabbbccc"}
 print(D)
 D1 = {'aa': 11, 'bb': 22}
 D2 = {}
-D2['aa'] = 11
 D2['bb'] = 22
+D2['cc'] = 33
+D2['aa'] = 11
 D3 = dict(aa=11, bb=22)
 print(D1, D2, D3)
 
-Ks = list(D1.keys())
+Ks = list(D2.keys())
 Ks.sort()
 print(Ks)
 for k in Ks:
-    print(k, '=>', D1[k])
-for k in sorted(D1):
-    print(k, '=>', D1[k])
+    print(k, '=>', D2[k])
+for k in sorted(D2):
+    print(k, '=>', D2[k])
 
 x = 5
 while x > 0:
@@ -69,8 +71,8 @@ while x > 0:
 
 if not 'cc' in D3:
     print("nie ma")
-v1 = D2.get('cc', 0)
-v2 = D2['cc'] if 'cc' in D2 else 1
+v1 = D3.get('cc', 0)
+v2 = D3['cc'] if 'cc' in D3 else 1
 print(v1, v2)
 
 
